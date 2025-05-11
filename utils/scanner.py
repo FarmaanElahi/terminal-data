7014d94ca576a9b5b181cc06d44f73fa863edbb5
Farmaan Elahi <devfarmaan@gmail.com>
2025-05-12 01:08:45 +0530

    Completed upgrade

--- utils/scanner.py
+++ utils/scanner.py
@@ -1,44 +1,41 @@
 import pandas as pd
 
+from utils.bucket import storage_options, data_bucket
 from utils.fundamentals import get_fundamentals
 from utils.industry import get_industry_classification
+from utils.pandas_utils import make_df_ready_for_serialization
 from utils.pandas_utils import merge_df_safely
 from utils.rating import get_ratings
 from utils.technical import get_technicals
 from utils.tradingview import TradingView
-from utils.pandas_utils import make_df_ready_for_serialization
-from utils.bucket import storage_options, data_bucket
-from logging import Logger
-
-logger = Logger(__name__)
 
 
 async def run_full_scanner_build():
-    logger.info("Running full scanner build")
+    print("Running full scanner build")
 
-    df = TradingView.get_base_symbols(1000)
-    logger.info("Base Symbols loaded")
+    df = TradingView.get_base_symbols(100)
+    print("Base Symbols loaded")
 
     index_df = await  TradingView.get_index(df.columns)
-    logger.info("Index loaded")
+    print("Index loaded")
 
     df = pd.concat([df, index_df])
-    logger.info("Final symbols generated")
+    print("Final symbols generated")
 
     df = merge_df_safely(df, get_industry_classification())
-    logger.info("Industry classification updated")
+    print("Industry classification updated")
 
     df = merge_df_safely(df, get_fundamentals())
-    logger.info("Fundamentals updated")
+    print("Fundamentals updated")
 
     df = merge_df_safely(df, await get_technicals(df, df.index.to_list()))
-    logger.info("Technicals updated")
+    print("Technicals updated")
 
     df = merge_df_safely(df, get_ratings(df))
-    logger.info("Rating updated")
+    print("Rating updated")
 
     df = make_df_ready_for_serialization(df)
-    logger.info("Prepare for serialization")
+    print("Prepare for serialization")
 
     df.to_parquet(f'oci://{data_bucket}/symbols-full-v2.parquet', compression='zstd', storage_options=storage_options)
-    logger.info("Scanner build complete")
+    print("Scanner build complete")
