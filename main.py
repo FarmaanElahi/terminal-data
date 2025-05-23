a4bc320dce872c3d927a32cf9b61927ab756d048
Farmaan Elahi <devfarmaan@gmail.com>
2025-05-24 02:24:02 +0530

    Refactored and formatted scanner code

--- main.py
+++ main.py
@@ -8,13 +8,13 @@ from dotenv import load_dotenv
 load_dotenv()
 
 
-async def run():
+def run():
     parser = argparse.ArgumentParser(
         description="CLI tool for downloading fundamentals or running scanner."
     )
     parser.add_argument(
         "--mode",
-        choices=["download", "scan", "alerts"],
+        choices=["download", "scan", "alerts", "scanner"],
         required=True,
         help="Choose 'download' to fetch fundamentals or 'scan' to run the scanner."
     )
@@ -23,13 +23,16 @@ async def run():
 
     if args.mode == "download":
         from utils.fundamentals import download_fundamentals
-        return await download_fundamentals()
+        return asyncio.run(download_fundamentals())
     if args.mode == "scan":
         from utils.scanner import run_full_scanner_build
-        return await run_full_scanner_build()
+        return asyncio.run(run_full_scanner_build())
     if args.mode == "alerts":
         from modules.alerts.worker import run_alerts_worker
-        return await run_alerts_worker()
+        return asyncio.run(run_alerts_worker())
+    if args.mode == "scanner":
+        from modules.scanner.main import run
+        return run()
     print("[ERROR] Invalid mode. Use 'download' or 'scan'.")
     return None
 
@@ -40,7 +43,7 @@ def main():
         level=logging.INFO,
         format='%(asctime)s - %(name)s - %(levelname)s - %(message)s'
     )
-    asyncio.run(run())
+    run()
 
 
 if __name__ == "__main__":
