4ad4b057d98178e1e57f27b59bd02f612a9645c8
Farmaan Elahi <devfarmaan@gmail.com>
2025-07-19 19:34:31 +0530

    Added stockbee criteria

--- utils/technical.py
+++ utils/technical.py
@@ -511,9 +511,16 @@ def momentum(d: pd.DataFrame, short_period=20, long_period=50, accel_periods=(5,
 def stockbee(d: pd.DataFrame):
     return {
         "c_by_min_c_7": d.close / d.close.rolling(7).min(),
+        "c/minc7": d.close / d.close.rolling(7).min(),
         "c_by_min_c_10": d.close / d.close.rolling(10).min(),
+        "c/minc10": d.close / d.close.rolling(10).min(),
         "c_by_min_c_14": d.close / d.close.rolling(14).min(),
+        "c/minc14": d.close / d.close.rolling(14).min(),
         "c_by_min_c_21": d.close / d.close.rolling(21).min(),
+        "c/minc21": d.close / d.close.rolling(21).min(),
         "c_by_min_c_30": d.close / d.close.rolling(30).min(),
-        "avgc7_by_avgc65": d.close.rolling(7).mean() / d.close.rolling(65).mean()
+        "c/minc30": d.close / d.close.rolling(30).min(),
+        "c/avgc126": d.close / d.close.rolling(126).mean(),
+        "avgc7_by_avgc65": d.close.rolling(7).mean() / d.close.rolling(65).mean(),
+        "avgc7/avgc65": d.close.rolling(7).mean() / d.close.rolling(65).mean(),
     }
