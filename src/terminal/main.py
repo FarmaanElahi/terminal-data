b54a16b849aef8b3a51833f04caca21d5ac38ada
Farmaan Elahi <devfarmaan@gmail.com>
2026-02-19 10:38:23 +0530

    refactored loggin name

--- src/terminal/main.py
+++ src/terminal/main.py
@@ -1,15 +1,12 @@
-import logging
 import warnings
 
 from fastapi import FastAPI
 
 from .api import api_router as api_router
-from .log_config import configure_logging
+from .logging import configure_logging
 
 # Filter out Pydantic migration warnings
 warnings.filterwarnings("ignore", message=".*has been moved to.*")
-
-log = logging.getLogger(__name__)
 
 # we configure the logging level and format
 configure_logging()
